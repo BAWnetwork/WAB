312b26124ae5c69ad3f7f31009aa4fdd1fec923f
WAB Network <40134479+WABnetwork@users.noreply.github.com>
2018-06-26 01:41:31 +0700

    Add files via upload

---
+++ wabTokenSend.js
@@ -0,0 +1,12 @@
+let TokenSend = require("../interface/transaction.js").TokenSend;
+let wabContract = require("../contract/contract.js").wabContract;
+class wabTokenSend extends TokenSend
+{
+    constructor(from,to,tokenAddress,CoinAmount,gas,gasPrice,nonce)
+    {
+        super(from,tokenAddress,gas,gasPrice,nonce);
+        this.Contract = new wabContract(tokenAddress);
+        this.trans.setData(this.Contract.getData(to,CoinAmount));
+    }
+}
+module.exports = wabTokenSend;
